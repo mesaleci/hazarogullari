f3f29c7b33442eb1b9e2efb74ce836f4d6cbfb92
Kürşat Öztürk <46288506+mesaleci@users.noreply.github.com>
2019-02-10 22:44:08 +0300

    Add files via upload
    PHP 'de yorum satırı ve kullanımı. Kodlar üzerinde değişiklikler yaparak projelerinizde kullanabilir ve kendinizi geliştirebilirsiniz.

---
+++ yorumsatiri.php
@@ -0,0 +1,16 @@
+<?php
+echo "Meşaleciler";
+
+# Yorum satırı. 
+
+// Yorum satırı. 
+
+/* Yorum
+   satırı
+*/   
+
+/* Ben en çok bu yorum satırını kullanmaktayım zira hem daha fonksiyonel
+hemde daha detaylı bir anlatım için daha ideal. Sizlere de tavsiyem bu
+fonksiyonu kullanmanızdır. */
+
+?>

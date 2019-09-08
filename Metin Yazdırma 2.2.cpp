bac30617128a39f8485c90782bb90433125814e8
Kürşat Öztürk <46288506+mesaleci@users.noreply.github.com>
2019-09-08 15:25:40 +0300

    Add files via upload
    Yazılım C dersinde kullanılan kaynak koddur.

---
+++ Metin Yazdırma 2.2.cpp
@@ -0,0 +1,15 @@
+/* 1th GED Regiment Software Department 2.2*/ 
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	printf("Hello ");
+	printf("Mesaleciler!\n");
+	system("pause");
+	
+	return 0;
+}
+
+
+
